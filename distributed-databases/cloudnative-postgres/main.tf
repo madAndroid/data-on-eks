47b491396cbd6b8133fe0409430930cc0da418fc
Andrew Stangl <andrewstangl@gmail.com>
2023-09-18 16:26:28 +0200

    Only handle node groups via submodule

--- distributed-databases/cloudnative-postgres/main.tf
+++ distributed-databases/cloudnative-postgres/main.tf
@@ -11,16 +11,8 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  manage_aws_auth_configmap = true
+  manage_aws_auth_configmap = false
   # create_aws_auth_configmap = false
-
-  aws_auth_roles = [
-    {
-      rolearn  = "arn:aws:iam::482649550366:role/AdministratorAccess"
-      username = "Administrator"
-      groups   = ["system:masters"]
-    },
-  ]
 
   #---------------------------------------
   # Note: This can further restricted to specific required for each Add-on and your application
@@ -64,61 +56,7 @@ module "eks" {
       AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
     }
   }
-  eks_managed_node_groups = {
-    #  We recommend to have a MNG to place your critical workloads and add-ons
-    #  Then rely on Karpenter to scale your workloads
-    #  You can also make uses on nodeSelector and Taints/tolerations to spread workloads on MNG or Karpenter provisioners
-    # doeks_node_group = {
-    #   name        = "doeks-node-group"
-    #   description = "EKS managed node group example launch template"
 
-    #   ami_id = data.aws_ami.eks.image_id
-    #   # This will ensure the bootstrap user data is used to join the node
-    #   # By default, EKS managed node groups will not append bootstrap script;
-    #   # this adds it back in using the default template provided by the module
-    #   # Note: this assumes the AMI provided is an EKS optimized AMI derivative
-    #   enable_bootstrap_user_data = true
-
-    #   # Optional - This is to show how you can pass pre bootstrap data
-    #   pre_bootstrap_user_data = <<-EOT
-    #     echo "Node bootstrap process started by Data on EKS"
-    #   EOT
-
-    #   # Optional - Post bootstrap data to verify anything
-    #   post_bootstrap_user_data = <<-EOT
-    #     echo "Bootstrap complete.Ready to Go!"
-    #   EOT
-
-    #   subnet_ids = module.vpc.private_subnets
-
-    #   min_size     = 1
-    #   max_size     = 9
-    #   desired_size = 1
-
-    #   force_update_version = true
-    #   instance_types       = ["m5.xlarge"]
-
-    #   ebs_optimized = true
-    #   block_device_mappings = {
-    #     xvda = {
-    #       device_name = "/dev/xvda"
-    #       ebs = {
-    #         volume_size = 100
-    #         volume_type = "gp3"
-    #       }
-    #     }
-    #   }
-
-    #   labels = {
-    #     WorkerType    = "ON_DEMAND"
-    #     NodeGroupType = "doeks"
-    #   }
-
-    #   tags = {
-    #     Name = "doeks-node-grp"
-    #   }
-    # }
-  }
 }
 
 locals {
